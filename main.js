7f5f27cfa104e1973a5475e85273e2180db23ea9
taeheon <8269apk@naver.com>
2021-04-29 13:56:20 +0900

    commit

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ $(function(){
     $('#fullpage').fullpage({
 		//options here
 		autoScrolling:true,
-		scrollHorizontally: true
+		scrollHorizontally: true,
+		scrollingSpeed: 1500,
+		navigation: true,
+		navigationTooltips:['Home','about','Home','about','Home','about']
 	});
 
 });
